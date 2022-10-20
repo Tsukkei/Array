7decc92229b90ec25dcf4dc7d61047eef7253d76
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2022-10-20 15:04:57 +0800

    Create multiarrays.java

---
+++ multiarrays.java
@@ -0,0 +1,16 @@
+public class multiarray {
+    public static void main(String[] args) throws Exception {
+
+
+
+        char[][] Letters = {
+            {'A', 'B', 'C'},
+            {'D', 'E', 'F'},
+            {'G', 'H', 'I'},
+        };
+
+        System.out.println(Letters[1][1]);
+        System.out.println(Letters[2][0]);
+        System.out.println(Letters[0][1]);
+    }
+}
