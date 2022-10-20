a55c7c48a1e9292c49a10c8c3c6ac77b5bc3ecf3
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2022-10-20 15:46:07 +0800

    Create arraywithnested.java

---
+++ arraywithnested.java
@@ -0,0 +1,29 @@
+public class multiarray {
+    public static void main(String[] args) throws Exception {
+
+        char[][] Letters3x3 = {
+            {'A', 'B', 'C'},
+            {'D', 'E', 'F'},
+            {'G', 'H', 'I'},
+        };
+
+        // System.out.println(Letters[1][1]);
+        // System.out.println(Letters[2][0]);
+        // System.out.println(Letters[0][1]);
+   
+  // outer loop
+  for (int row = 0; row < Letters3x3.length; row++) {
+
+      // inner loop
+      for (int column = 0; column < Letters3x3.length; column++) {
+            System.out.print(Letters3x3[row][column] + " ");
+
+        // for (int seconds = 0; seconds < 60; seconds++) {
+
+          
+                }
+            System.out.println(" ");
+            }
+        }
+    }
+// }
