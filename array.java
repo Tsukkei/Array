8a30ba853afc2c516f0213cbd8093b4ffce5eb4c
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2022-10-20 14:50:48 +0800

    Create array.java

---
+++ array.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Array of Strings
+        String[] myfavefastfood = { "Jollibee", "Mcdonalds", "KFC", "MangInasal",
+                "Chowking" };
+
+        // Array of Integers
+        int[] primeNumbers = { 2, 3, 5, 7 };
+
+        // Array of Characters
+        char[] myName = { 'E', 'A' , 'R' , 'L' };
+        
+         // Access first array element
+         System.out.println(myName[0]);
+
+ 
+     }
+}
